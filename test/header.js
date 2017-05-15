960ddfc01972d770827be8cc5e368cf14e831f3c
Anandesi <anandesiakshay@gmail.com>
2017-05-15 09:49:46 +0530

    test cases

--- test/header.js
+++ test/header.js
@@ -1,36 +1,36 @@
+
+
+import React from 'react';
+// import { mount, render, shallow } from 'enzyme';
+// import { expect } from 'chai';
 //
+// import { observer } from "mobx-react"
+// import { Popover, PopoverAnimationVertical } from 'material-ui/Popover'
+// //components
 //
-// import React from 'react';
-// // import { mount, render, shallow } from 'enzyme';
-// // import { expect } from 'chai';
-// //
-// // import { observer } from "mobx-react"
-// // import { Popover, PopoverAnimationVertical } from 'material-ui/Popover'
-// // //components
-// //
-// import Header from '../public/app/components/header.js';
-// // import HideColumns from '../public/app/components/headerComponents/hideColumnsComponent.js';
-// // import FilterRows from '../public/app/components/headerComponents/filterRowsComponent.js';
-// // import Search from '../../tables-ui/public/app/components/headerComponents/searchComponent.js';
-// // import HeaderTable from '../public/app/components/headerComponents/headerTableComponent.js';
-// //
+import Header from '../public/app/components/header.js';
+// import HideColumns from '../public/app/components/headerComponents/hideColumnsComponent.js';
+// import FilterRows from '../public/app/components/headerComponents/filterRowsComponent.js';
+// import Search from '../../tables-ui/public/app/components/headerComponents/searchComponent.js';
+// import HeaderTable from '../public/app/components/headerComponents/headerTableComponent.js';
 //
-//
-//  describe('<HEADERRRRRRRRRRRRRRRRs/>', () => {
-//
-//
-//
-//
-//        it('Component is rendering', () => {
-//
-//           wrapper = shallow(< Header  />);
-//           var table = {
-//             'id': { label: function() { return '12345'; } },
-//             'title': { label: function() { return 'akshay'; } }
-//
-//           };
-//          expect(wrapper).to.exist;
-//      });
-//
-//
-//   });
+
+
+ describe('<HEADERRRRRRRRRRRRRRRRs/>', () => {
+
+
+
+
+       it('Component is rendering', () => {
+
+          wrapper = shallow(< Header  />);
+          var table = {
+            'id': { label: function() { return '12345'; } },
+            'title': { label: function() { return 'akshay'; } }
+
+          };
+         expect(wrapper).to.exist;
+     });
+
+
+  });
--- test/headertable/filterRowComponent.js
+++ test/headertable/filterRowComponent.js
@@ -1,27 +1,45 @@
-import React from 'react';
-// import { mount, render, shallow } from 'enzyme';
-// import { expect } from 'chai';
-
-
-//components
-
-import FilterRow from '../../public/app/components/headerComponents/filterRowComponent.js';
-
-
-
-describe('<filterRowComponent.js/>', () => {
-
-
-
-  let wrapper;
-    before(() => {
-      wrapper = shallow(< FilterRow/>);
-    });
-
-    it('Component is rendering', () => {
-      expect(wrapper).to.exist;
-  });
-
-
-
-});
+// import React from 'react';
+// // import { mount, render, shallow } from 'enzyme';
+// // import { expect } from 'chai';
+//
+//
+// //components
+//
+// import FilterRow from '../../public/app/components/headerComponents/filterRowComponent.js';
+//
+//
+// let dataTypes = this.props.tableStore.getColumns
+// 						.filter((x)=>{
+// 							return !(x.dataType == 'Id' || x.dataType == 'ACL')
+// 						})
+// 						.map((data,i)=>{
+// 							if(data.dataType == 'List'){
+// 								return <option key={ i } value={ data.name } type={ data.dataType } data-relatedTo={ data.relatedTo }>{ data.name }</option>
+// 							} else {
+// 								return <option key={ i } value={ data.name } type={ data.dataType }>{ data.name }</option>
+// 							}
+// describe('<filterRowComponent.js/>', () => {
+//
+//
+//
+//   // let wrapper;
+//   //   before(() => {
+//   //     wrapper = shallow(< FilterRow/>);
+//   //   });
+//
+//     it('Component is rendering', () => {
+//
+//       var filter(x) = {
+//
+//                  'id': { label: function() { return !(x.dataType == 'Id' || x.dataType == 'ACL') ; } },
+//                  'title': { label: function() { return 'akshay'; } }
+//
+//                };
+//
+//
+//       expect(wrapper).to.exist;
+//   });
+//
+//
+//
+// });
